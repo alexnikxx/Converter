3d337caa79ff32bae283e5c7343f52d4c0dbe3da
alexnikxx <alexnikxx@gmail.com>
2024-12-15 21:20:05 +0300

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Aleksandra Nikiforova on 15/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
